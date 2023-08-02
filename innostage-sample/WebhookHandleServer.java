bbf8e167d95f701eb9b65e36376c3038dda758c6
root <root@karavan-6bfb65b5b-dgshp>
2023-08-02 07:20:21 +0000

    02.08.2023, 10:20:18

--- innostage-sample/WebhookHandleServer.java
+++
@@ -1,9 +0,0 @@
-import org.apache.camel.builder.RouteBuilder;
-
-public class WebhookHandleServer extends RouteBuilder {
-  @Override
-  public void configure() throws Exception {
-    from("platform-http:/hello?httpMethodRestrict=GET")
-      .setBody(simple("Hello Innostage!!!"));
-  }
-}
