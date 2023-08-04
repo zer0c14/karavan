8ba407e5c5f97814ad3c9ddccdb44e60709cdf50
root <root@karavan-6bfb65b5b-dgshp>
2023-08-04 08:48:18 +0000

    04.08.2023, 11:48:16

--- innostage-sample/MyRouteBuilder.java
+++ innostage-sample/MyRouteBuilder.java
@@ -32,7 +32,7 @@ public class MyRouteBuilder extends RouteBuilder {
   }
 
   public void configure() {
-    from("netty-http:http://localhost:8000/pt-af/v1/create-session")
+    from("netty-http:http://localhost:12345/pt-af/v1/create-session")
         .process(exchange -> exchange.setMessage(new DefaultMessage(exchange)))
         .to(
             "netty-http:https://10.70.19.2:8443/login"
